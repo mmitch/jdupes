3b814f494dc24ec330318d391f0daed7566db5f4
Christian Garbs <mitch@cgarbs.de>
2017-03-25 21:03:10 +0100

    add multi-file support for print action

--- act_printmatches.c
+++ act_printmatches.c
@@ -11,6 +11,7 @@
 extern void printmatches(file_t * restrict files)
 {
   file_t * restrict tmpfile;
+  filename_t * restrict filename;
   int printed = 0;
 
   while (files != NULL) {
@@ -19,11 +20,13 @@ extern void printmatches(file_t * restrict files)
       if (!ISFLAG(flags, F_OMITFIRST)) {
         if (ISFLAG(flags, F_SHOWSIZE)) printf("%" PRIdMAX " byte%c each:\n", (intmax_t)files->size,
          (files->size != 1) ? 's' : ' ');
-        fwprint(stdout, files->filename->d_name, 1);
+        for (filename = files->filename; filename; filename = filename->next)
+          fwprint(stdout, filename->d_name, 1);
       }
       tmpfile = files->duplicates;
       while (tmpfile != NULL) {
-        fwprint(stdout, tmpfile->filename->d_name, 1);
+        for (filename = tmpfile->filename; filename; filename = filename->next)
+          fwprint(stdout, filename->d_name, 1);
         tmpfile = tmpfile->duplicates;
       }
       if (files->next != NULL) fwprint(stdout, "", 1);
